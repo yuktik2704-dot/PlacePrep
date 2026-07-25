f02a9baa208b03b1d8f4396f80627bd197b24f70
Yukti Kathuria <yuktik2704@gmail.com>
2026-07-25 19:40:50 +0530

    Fix upload API URL

--- Frontend/js/upload.js
+++ Frontend/js/upload.js
@@ -171,7 +171,7 @@ if (!result.ok) {
 
             const response=await fetch(
 
-                "http://127.0.0.1:5000/api/resume/upload",
+                "https://placeprep-sb9x.onrender.com/api/resume/upload",
 
                 {
 
